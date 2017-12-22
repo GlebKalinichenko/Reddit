1814b079ede70bcfc13ba02d9b40666506d2f3ed
GlebKalinichenko <gleb.kalinichenko@umbrella-web.com>
2017-12-22 19:38:40 +0300

    Implement injecting of post list use case

--- app/src/main/java/com/example/knifestart/reddit/di/listposts/ListPostsModule.kt
+++ app/src/main/java/com/example/knifestart/reddit/di/listposts/ListPostsModule.kt
@@ -1,8 +1,12 @@
 package com.example.knifestart.reddit.di.listposts
 
+import com.example.knifestart.domain.FetchPostsUseCase
+import com.example.knifestart.domain.IPostRepository
 import com.example.knifestart.reddit.di.scope.PostScope
 import dagger.Module
 import dagger.Provides
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by glebkalinichenko on 21.12.17.
@@ -11,4 +15,8 @@ import dagger.Provides
 class ListPostsModule {
     @Provides @PostScope
     fun provideTest() = 1.0
+
+    @Provides @PostScope
+    fun provideFetchListPostsUseCase(postRepository: IPostRepository) : FetchPostsUseCase =
+            FetchPostsUseCase(Schedulers.io(), AndroidSchedulers.mainThread(), postRepository)
 }
